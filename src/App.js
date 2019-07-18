0dd5c2a4084f8d275b22772c3ee6c2b91cda312f
dcmarin91 <dcmarin91@hotmail.com>
2019-07-18 14:38:28 -0500

    Add integration with API

--- src/App.js
+++ src/App.js
@@ -1,29 +1,106 @@
 import React, { Component } from 'react';
+import "./index.css";
 
 /// Modifica el componente para que se puedan agregar tareas, tachar y destacharlas y error de validacion en el input
 
 class App extends Component {
-  constructor() {
-    super()
+
+  constructor(props) {
+    super(props)
     this.state = {
+      newTask: '',
       tasks: [
-        { id: 1, name: "Sacar la ropa", done: false },
-        { id: 2, name: "Hacer la cama", done: true },
-        { id: 3, name: "Leer un rato", done: false }
+        { id: 1, title: "Sacar la ropa", done: false },
+        { id: 2, title: "Hacer la cama", done: true },
+        { id: 3, title: "Leer un rato", done: false },
       ],
-      newTask: ''
+      errors: {
+        newTask: false
+      }
     }
   }
-  render() {
+
+  request = () =>{
+    fetch('http://makeitreal-todo.herokuapp.com/todo_items')
+    .then(function(response) {
+      return response.json();
+      })
+      .then(myJson => {
+        this.setState({tasks: myJson})
+      });
+    } 
+
+
+  addTask(event) {
+    if (this.validateNewTask()) {
+      let oldTasks = this.state.tasks
+      let newTask = {
+        id: Math.max(...oldTasks.map(task => task.id)) + 1,
+        title: this.state.newTask,
+        done: false
+      }
+      this.setState({
+        tasks: [...oldTasks, newTask],
+        newTask: ''
+      })
+    }
+    event.preventDefault();
+  }
+  
+  validateNewTask() {
+    if (this.state.newTask === '') {
+      this.setState({
+        errors: {
+          newTask: true
+        }
+      })
+      return false
+    }
+    return true
+  }
+  updateTask(event) {
+    this.setState({
+      newTask: event.target.value,
+      errors: {
+        newTask: false
+      }
+    })
+  }
+  toogleDone(id) {
+    const newTasks = this.state.tasks.map(task => {
+      if (task.id === id) {
+        task.done = !task.done
+        return task
+      }
+      return task
+    })
+    this.setState({
+      tasks: newTasks
+    })
+  }
+  renderTasks() {
+    const tasks = this.state.tasks.map((task, index) => {
+      return (
+        <li
+          className={task.done ? 'done' : null}
+          key={task.id}
+          onClick={this.toogleDone.bind(this, task.id)}>
+          {task.title}
+        </li>
+      )
+    })
+    return tasks
+  }
+   render() {
     return (
       <div className="wrapper">
         <div className="list">
-          <h3>Por hacer:</h3>
+          <h3 onClick={this.request()}>Por hacer:</h3>
           <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
+            {this.renderTasks()}
           </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
+          <form onSubmit={this.addTask.bind(this)}>
+          <input className={this.state.errors.newTask ? 'error' : null} type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} onChange={this.updateTask.bind(this)}/>
           </form>
         </div>
       </div>
